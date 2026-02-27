9b89d8860b3f146888585135e4902fb4488d5dba
Gonzalo Jaime <gjaimeguinazu@gmail.com>
2026-02-27 17:01:32 -0300

    add question mark icon

---
+++ src/components/icons/QuestionMark.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+export const QuestionMark = ({ width, height, stroke, strokeWidth }) => {
+  return (
+    <svg aria-hidden="true"
+        xmlns="http://www.w3.org/2000/svg"
+        width={width}
+        height={height}
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke={stroke}
+        strokeWidth={strokeWidth}
+        strokeLinecap="round"
+        strokeLinejoin="round"
+    >
+        <path d="M8 8a3.5 3 0 0 1 3.5 -3h1a3.5 3 0 0 1 3.5 3a3 3 0 0 1 -2 3a3 4 0 0 0 -2 4" />
+        <path d="M12 19l0 .01" />
+    </svg>
+
+  )
+}
